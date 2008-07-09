e7747a1a22bd7aac884a8076dd001708ddd87387
Justin White <just6979@yahoo.com>
2008-07-09 01:02:35 -0400

    added restart and refactored: case, var expansion

--- scgi-daemon.sh
+++ scgi-daemon.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
 
+## Justin White
+## July 2008
+
 ## this little proggy uses debian's start-stop-daemon
-## to get the scgi server going and stopping
+## to get the scgi server going and stopping, and restarting
+## like rc.d scripts, but this is mine, for practice
 
 cmd=$1
 
-name='scgi-server'
-gid='www'
-uid='www'
-dir='/home/www/main/'
-exe='/home/www/main/scgi-server.py'
-pid='scgi-server.pid'
-log='scgi-server.log'
+name="scgi-server"
 
-if [[ $cmd = 'start' ]]
-then
+gid="www"
+uid="www"
+
+dir="/home/www/main"
+exe="$dir/$name.py"
+pid="$dir/$name.pid"
+log="$dir/$name.log"
+
+starter() {
+	echo -n Starting $name...
 	start-stop-daemon -v -g $gid -c $uid -d $dir -mp $pid -Sba $exe
-else
-	if [[ $cmd = 'stop' ]]
-	then
-		start-stop-daemon -v -p $pid -K
-		rm -f $pid
-	else
-		echo "Unknown request: '$cmd'"
-	fi
-fi
+	echo Done
+}
 
-echo 'Done'
+stopper() {
+	echo -n Stopping $name...
+	start-stop-daemon -v -p $pid -K
+	rm -f $pid
+	echo Done
+}
+
+case "$cmd" in
+"start")
+	starter
+;;
+"stop")
+	stopper
+;;
+"restart")
+	stopper
+	starter
+;;
+*)
+	echo "Unknown request: '$cmd'"
+;;
+esac
